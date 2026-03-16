8e8bc4f73510a7bde6bc81ea1e7da2d9afbabc3b
Shapathaan <shapathaan@gmail.com>
2026-03-17 03:39:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const intro = document.getElementById("intro");
+const main = document.getElementById("main");
+const video = document.getElementById("introVideo");
 
+if(video){
+
+video.onended = () => {
+
+intro.classList.add("hideIntro");
+
+setTimeout(()=>{
+main.classList.add("showMain");
+},700);
+
+}
+
+}
